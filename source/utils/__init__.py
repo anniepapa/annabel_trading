e03b834391a7531a9da1592bfab07ddc3c035c2d
anniepapa <chen613nl@gmail.com>
2023-06-17 13:08:50 +0200

    wrap up quotecast, fetch product price data

--- source/utils/__init__.py
+++ source/utils/__init__.py
@@ -1,2 +1,5 @@
 from .degiro_connector import DegiroConnection  # noqa
 from .trading_operator import TradingOperator  # noqa
+from .product_consumer import (  # noqa
+    FinancialProductConsumer as ProductConsumer,
+)
