0cb6d79e34f79efc2a40e5ac469352076188b82b
anniepapa <chen613nl@gmail.com>
2023-07-14 22:39:00 +0200

    solve sign issue

--- source/models/livermore.py
+++ source/models/livermore.py
@@ -158,11 +158,9 @@ class LivermoreTradingRule(TradingAnalyzor):
         net = decimalize(earns - fees * 2)
         percent = decimalize(net / earns) * (net / abs(net))
 
-        logger.info(f"{percent, net, earns}")
-
         logger.info(
             f"✍✍ Earning: {earns}, needs to pay: {fees*2}. Net: {net}. "
-            f"earn percent: {net/abs(net)*percent*100}%... "
+            f"earn percent: {percent*100}%... "
             f"{self.ratio_diff_sell} ratio sell: {self.ratio_diff_sell*100}%"
         )
 
