07c1019ff70d7e384bec632f41f4686ba98f400b
anniepapa <chen613nl@gmail.com>
2023-07-26 22:17:54 +0200

    more readable logs

--- source/my_logger.py
+++ source/my_logger.py
@@ -2,7 +2,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format="%(asctime)s-%(name)s 🔱 %(module)s-%(levelname)s:\n %(message)s\n-------------------💲",  # noqa
+    format="%(asctime)s-%(name)s 🔱 %(module)s-%(levelname)s 📢 %(message)s 📑",  # noqa
 )
 
 logger = logging.getLogger(__name__)
